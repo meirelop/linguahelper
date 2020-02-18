5a01cae35304711c47cc05c995f59816c70cd86d
Bekzhan <rakhmetzhan.bekzhan@gmail.com>
2020-02-18 23:37:36 +0600

    correction docstring

--- src/main.py
+++ src/main.py
@@ -1,14 +1,19 @@
 import telebot
 import xlrd
 from random import (randint, random)
+
+
+"""This function selects a random word from an exsel file."""
 def rand():
-    rb = xlrd.open_workbook('/Users/admin/Projects/linguahelper/Saved translations.xlsx')
-    sheet = rb.sheet_by_index(0)
-    rownum = randint(0, sheet.nrows)
-    return sheet.cell_value(rownum, 0)+' ----- '+sheet.cell_value(rownum, 1) + '\n' +sheet.cell_value(rownum, 2)+ ' ----- ' + sheet.cell_value(rownum, 3)
-    """Эта функция  выбирает случайного слова с Exsel файла."""
-Token = "  "
-bot = telebot.TeleBot(Token)
+      rb = xlrd.open_workbook('/Users/admin/Projects/linguahelper/Saved translations.xlsx')
+      sheet = rb.sheet_by_index(0)
+      rownum = randint(0, sheet.nrows)
+      return sheet.cell_value(rownum, 0)+' ----- '+sheet.cell_value(rownum, 1) + '\n' +sheet.cell_value(rownum, 2)+ ' ----- ' + sheet.cell_value(rownum, 3)
+
+
+TOKEN = "  "
+bot = telebot.TeleBot(TOKEN)
+
 
 def randtwo(ls, sr):
     rb = xlrd.open_workbook('/Users/admin/Projects/linguahelper/Saved translations.xlsx')
@@ -27,15 +32,17 @@ def randtwo(ls, sr):
 def start(message):
     bot.send_message(message.chat.id, rand())
 
-
-@bot.message_handler(commands=['frenchenglish'])  #  french - english
+'''This function randomly selects and displays words from an Exsel file,only translate words from French to English.'''
+@bot.message_handler(commands=['frenchenglish'])
 def englishfrench(message):
     bot.send_message(message.chat.id, randtwo('French','English'))
-    ''' Это функция рандомно выбирает и показываеть слова с Exsel файла только перевод слов с французкого на английский .'''
 
-@bot.message_handler(commands=['englishfrench']) # english -frensh
+
+'''This function randomly selects and displays words from an Exsel file,only translating words from English to French.'''
+@bot.message_handler(commands=['englishfrench'])
 def frenchenglish(message):
     bot.send_message(message.chat.id, randtwo('English','French'))
-    ''' Это функция рандомно выбирает и показываеть слова с Exsel файла только перевод слов с Английского на Французкий .'''
+
+
 bot.polling()
 
