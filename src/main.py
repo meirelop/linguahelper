943dae77f8321fdb27d3fb305a7231a279406476
meirkhan <meirkhan@MacBook-Pro-de-meirkhan.local>
2020-02-21 11:53:53 +0100

    cosmetic changes

--- src/main.py
+++ src/main.py
@@ -1,28 +1,28 @@
 import telebot
 import xlrd
-from random import (randint, random)
+from random import randint
 
+
+BOT_TOKEN = ''
+bot = telebot.TeleBot(BOT_TOKEN)
+bot.delete_webhook()
 
 def rand():
-    '''
+    """
     Get randomly one translation from given file
        Returns
        -------
        str
            Random translated phrase/sentence
-    '''
+    """
     rb = xlrd.open_workbook('../inputs/Savedtranslations.xlsx')
     sheet = rb.sheet_by_index(0)
     rownum = randint(0, sheet.nrows)
     return sheet.cell_value(rownum, 0)+' ----- '+sheet.cell_value(rownum, 1) + '\n' +sheet.cell_value(rownum, 2)+ ' ----- ' + sheet.cell_value(rownum, 3)
 
 
-TOKEN = " "
-bot = telebot.TeleBot(TOKEN)
-
-
 def randtwo(ls, sr):
-    '''
+    """
     Get randomly one translation from given file
        Parameters
        ----------
@@ -35,15 +35,15 @@ def randtwo(ls, sr):
        -------
        str
            Random translated phrase/sentence
-    '''
+    """
     rb = xlrd.open_workbook('../inputs/Savedtranslations.xlsx')
     sheet = rb.sheet_by_index(0)
     rownum = randint(0, sheet.nrows)
     while (True):
-     if(ls==sheet.cell_value(rownum, 0) and sr==sheet.cell_value(rownum, 1)):
-        break
-     else:
-        rownum = randint(0, sheet.nrows)
+        if ls == sheet.cell_value(rownum, 0) and sr == sheet.cell_value(rownum, 1):
+            break
+        else:
+            rownum = randint(0, sheet.nrows)
     return sheet.cell_value(rownum, 0) + ' ----- ' + sheet.cell_value(rownum, 1) + '\n' +sheet.cell_value(rownum, 2)+ ' ----- ' + sheet.cell_value(rownum, 3)
 
 
@@ -54,22 +54,22 @@ def start(message):
 
 @bot.message_handler(commands=['frenchenglish'])
 def englishfrench(message):
-    ''' function randomly selects and displays words from an Exsel file,only
+    """
+    Function randomly selects and displays words from an Exsel file,only
     translate words from French to English.
-    '''
+    """
     bot.send_message(message.chat.id, randtwo('French','English'))
 
 
 @bot.message_handler(commands=['englishfrench'])
 def frenchenglish(message):
-    ''' function randomly selects and displays words from an Exsel file,
+    """
+    Function randomly selects and displays words from an Exsel file,
     only translating words from English to French.
-    '''
+    """
     bot.send_message(message.chat.id, randtwo('English','French'))
-
-#bot.delete_webhook()
 
 
 if __name__ == '__main__':
-     bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
 
