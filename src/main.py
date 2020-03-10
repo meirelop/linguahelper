140c952c7f1f0056634bc6f5beb4b07d7c68abce
Bekzhan <rakhmetzhan.bekzhan@gmail.com>
2020-03-10 12:08:06 +0600

    add choice languages

--- src/main.py
+++ src/main.py
@@ -3,6 +3,14 @@ import xlrd
 import os
 from random import (randint, random)
 
+rb = xlrd.open_workbook('../inputs/Savedtranslations.xlsx')
+sheet = rb.sheet_by_index(0)
+mysheet = []
+for i in range(sheet.nrows):
+    mysheet.append([sheet.cell_value(i, 0), sheet.cell_value(i, 1), sheet.cell_value(i, 2), sheet.cell_value(i, 3)])
+n = len(mysheet)
+print(n)
+''' All data from the exsel file is inserted into the mysheet '''
 
 def rand():
     '''
@@ -12,40 +20,48 @@ def rand():
        str
            Random translated phrase/sentence
     '''
-    rb = xlrd.open_workbook('../inputs/Savedtranslations.xlsx')
-    sheet = rb.sheet_by_index(0)
-    rownum = randint(0, sheet.nrows)
-    return sheet.cell_value(rownum, 0)+' ----- '+sheet.cell_value(rownum, 1) + '\n' +sheet.cell_value(rownum, 2)+ ' ----- ' + sheet.cell_value(rownum, 3)
+    rm = randint(0, n)
+    return mysheet[rm][0] + ' ----- ' + mysheet[rm][1] + '\n' + mysheet[rm][2] + ' ----- ' + mysheet[rm][3]
+
+def checklang():
+    lan = set()
+    for i in range(n):
+       lan.add(str(mysheet[i][0]) + " ---- " + str(mysheet[i][1]))
+    return lan
+    ''' 
+     
+     Returns
+       -------
+      function returned set languages '''
+
+def randtwo(ls, sr):
+    '''
+       Get randomly one translation from given file
+          Parameters
+          ----------
+          ls: str
+              Language to translate from
+          sr: str
+              Language to translate to
+          Returns
+          -------
+          str
+              Random translated phrase/sentence
+       '''
+    rm = randint(0, n-1)
+    print(rm)
+    while (True):
+        if(ls == mysheet[rm][0] and sr == mysheet[rm][1]):
+            break
+        else:
+            rm = randint(0, n-1)
+    return mysheet[rm][0] + ' ----- ' + mysheet[rm][1] + '\n' + mysheet[rm][2] + ' ----- ' + mysheet[rm][3]
+
 
 
 TOKEN = "1001230120:AAHB5gaj02BOsMTENcNDBGJFgKOzNYm4L70"
 bot = telebot.TeleBot(TOKEN)
 
-
-def randtwo(ls, sr):
-    '''
-    Get randomly one translation from given file
-       Parameters
-       ----------
-       ls: str
-           Language to translate from
-       sr: str
-           Language to translate to
-
-       Returns
-       -------
-       str
-           Random translated phrase/sentence
-    '''
-    rb = xlrd.open_workbook('../inputs/Savedtranslations.xlsx')
-    sheet = rb.sheet_by_index(0)
-    rownum = randint(0, sheet.nrows)
-    while (True):
-     if(ls==sheet.cell_value(rownum, 0) and sr==sheet.cell_value(rownum, 1)):
-        break
-     else:
-        rownum = randint(0, sheet.nrows)
-    return sheet.cell_value(rownum, 0) + ' ----- ' + sheet.cell_value(rownum, 1) + '\n' +sheet.cell_value(rownum, 2)+ ' ----- ' + sheet.cell_value(rownum, 3)
 
 
 @bot.message_handler(commands=['start'])
@@ -68,9 +84,24 @@ def frenchenglish(message):
     '''
     bot.send_message(message.chat.id, randtwo('English','French'))
 
-#bot.delete_webhook()
+@bot.message_handler(commands=["listoflanguages"])
+def handle_start(message):
+    '''list of language combinations'''
+    user_markup =telebot.types.ReplyKeyboardMarkup()
+    for i in checklang():
+        user_markup.row(i)
+    bot.send_message(message.chat.id,'Selected language!!!',reply_markup=user_markup)
 
+
+
+@bot.message_handler(content_types=['text'])
+def main(message):
+    '''choice of language combinations'''
+    checklan = str(message.text)
+    lans = checklan.split(' ---- ')
+    bot.send_message(message.chat.id, randtwo(lans[0], lans[1]))
 
 if __name__ == '__main__':
      bot.polling(none_stop=True)
+
 
